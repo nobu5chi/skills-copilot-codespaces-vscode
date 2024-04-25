310c651dc6afda8459ac59e073fb01a3652d603e
Nobuyuki Kamigochi <42070266+nobu5chi@users.noreply.github.com>
2024-04-25 05:27:41 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,5 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the fs module to read the file and send it to the client.
+// The file is located in the public directory.
+// The server should respond to GET requests to the /comments URL.
+
